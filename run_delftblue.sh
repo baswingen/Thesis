4daab9638003189d3adf305cd50e6d7e7c5a18de
baswingen <baswingen@gmail.com>
2026-03-18 13:12:25 +0100

    updates to .sh

--- run_delftblue.sh
+++ run_delftblue.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=run_model
-#SBATCH --partition=gpu-a100-small
+#SBATCH --partition=gpu-a100
 #SBATCH --time=00:15:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=8
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=1G
 #SBATCH --account=education-ME-MSc-ME
