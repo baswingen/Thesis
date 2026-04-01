b9f6d4b06fb040dfa0e28e396d10e1f6ac3aa121
baswingen <baswingen@gmail.com>
2026-04-01 17:19:07 +0200

    upgraded job

--- run_delftblue.sh
+++ run_delftblue.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=run_model
-#SBATCH --partition=gpu-a100-small
-#SBATCH --time=2:00:00
+#SBATCH --partition=gpu-a100
+#SBATCH --time=24:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=32
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=4G
 #SBATCH --account=education-me-msc-me
