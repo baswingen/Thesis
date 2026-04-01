74dd94205e8a06776b6d33700e7c9b4aedcb153a
baswingen <baswingen@gmail.com>
2026-04-01 09:59:49 +0200

    small job update

--- run_delftblue.sh
+++ run_delftblue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=run_model
 #SBATCH --partition=gpu-a100-small
-#SBATCH --time=24:00:00
+#SBATCH --time=2:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --gpus-per-task=1
